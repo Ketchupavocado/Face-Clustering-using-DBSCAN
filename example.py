511ac128f71450be7bdf7aa95f0a220543a4088c
Student <kaan@erman.org>
2025-07-14 10:02:50 -0700

    Original Face Clustering

--- example.py
+++ example.py
@@ -1,12 +1,18 @@
 import get_encodings as enc
 import face_cluster as cluster
+#Time the code
+import time
 
-root_path = 'face_images/'
+
+root_path = 'Timer/'
 
 # Create a face database from existing faces
 dir_name, data_len = enc.create_face_database(root_path)
 
-# Apply face clustering to retrieve unique faces
+
+start_time = time.time()
 unq_faces, res_dir = cluster.do_cluster(dir_name)
-# print("Number of unique face detected: ", unq_faces)
-# print(f'You can find clustered face in "{res_dir}" directory')
+print("Number of unique face detected: ", unq_faces)
+print(f'You can find clustered face in "{res_dir}" directory')
+cluster_time = time.time() - start_time
+print(f"Face clustering completed in {cluster_time:.2f} seconds.")
